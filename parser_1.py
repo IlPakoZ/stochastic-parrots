72f7e60aa3520dfdeed8a67e69e95a564d103fcc
xJOTESTx J <pasquale.lobascioo@gmail.com>
2024-05-20 17:30:29 +0200

    Changed projectrant.py to read 4-chan data, minor modifications on the other files.

--- parser_1.py
+++ parser_1.py
@@ -16,6 +16,7 @@ for filename in os.listdir(dir):
         all_lines = all_lines[1:]
         last_comment = ""
         for line in all_lines:
+            line = line.lstrip()
             new_comm = re.match(r"--- [0-9]+", line)
             refs = re.match(r">>[0-9]+", line)
             if refs:
@@ -26,20 +27,30 @@ for filename in os.listdir(dir):
                     new_lines.append(last_comment)
                     last_comment = ""
                 new_lines.append(res)
-            elif line[0] == ">":
-                res = line[1:]
-                last_comment += res
+            #elif line[0] == ">":
+            #    res = line[1:]
+            #    last_comment += res
             else:
+                ls = line.split("\n")
+                for i in range(len(ls)):
+                    if ls[i]:
+                        if ls[i][:2] == ">>":
+                            ls[i] = ls[i][2:]
+                        elif ls[i][0] == ">":
+                            ls[i] = ls[i][1:]
+                line = "\n".join(ls)
+
                 last_comment += line
+
         new_lines.append(last_comment)
             
 
         #new_lines.append("<commentsend>")
         
 
-        out = open(dir+"res/"+filename, "w")
+        out = open(dir+"res/"+filename, "w", encoding="utf-8")
         out.writelines(new_lines)
         out.close()
-    except:
+    except UnicodeEncodeError:
         pass
 #print(all_lines)
