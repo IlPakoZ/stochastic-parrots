70887d8c89e3ad53a8fe12d0a49938bcd7f9902e
xJOTESTx J <pasquale.lobascioo@gmail.com>
2024-05-20 15:27:51 +0200

    miracolous fix wth???

--- parser_1.py
+++ parser_1.py
@@ -5,38 +5,41 @@ import os
 dir = "../r9k/"
 #filename = "r9k_72938968.txt"
 for filename in os.listdir(dir):
-    if not os.path.isfile(dir+filename):
-        continue
-    f = open(dir+filename, "r")
-    all_lines = f.readlines()
-    f.close()
-    new_lines = []
-    all_lines = all_lines[1:]
-    last_comment = ""
-    for line in all_lines:
-        new_comm = re.match(r"--- [0-9]+", line)
-        refs = re.match(r">>[0-9]+", line)
-        if refs:
+    try:
+        if not os.path.isfile(dir+filename):
             continue
-        if new_comm:
-            res = "<comment>\n"
-            if last_comment:
-                new_lines.append(last_comment)
-                last_comment = ""
-            new_lines.append(res)
-        elif line[0] == ">":
-            res = line[1:]
-            last_comment += res
-        else:
-            last_comment += line
-    new_lines.append(last_comment)
+        f = open(dir+filename, "r", encoding="utf-8")
+        #print(filename)
+        all_lines = f.readlines()
+        f.close()
+        new_lines = []
+        all_lines = all_lines[1:]
+        last_comment = ""
+        for line in all_lines:
+            new_comm = re.match(r"--- [0-9]+", line)
+            refs = re.match(r">>[0-9]+", line)
+            if refs:
+                continue
+            if new_comm:
+                res = "<comment>\n"
+                if last_comment:
+                    new_lines.append(last_comment)
+                    last_comment = ""
+                new_lines.append(res)
+            elif line[0] == ">":
+                res = line[1:]
+                last_comment += res
+            else:
+                last_comment += line
+        new_lines.append(last_comment)
+            
+
+        #new_lines.append("<commentsend>")
         
 
-    #new_lines.append("<commentsend>")
-    
-
-    out = open(dir+"res/"+filename, "w")
-    out.writelines(new_lines)
-    out.close()
-
+        out = open(dir+"res/"+filename, "w")
+        out.writelines(new_lines)
+        out.close()
+    except:
+        pass
 #print(all_lines)
