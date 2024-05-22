69fe7c6961cdd08472ec6dd6aef4774a60edb699
pako <pasquale.lobascioo@gmail.com>
2024-05-22 11:08:59 +0300

    Added chatbot interface and refactoring

--- project_rant.py
+++ project_rant.py
@@ -28,46 +28,54 @@ def load_dataset():
         freq_table = get_next_token_table(tokens, context_length, freq_table)
     return freq_table
 
-freq_table = load_dataset()
-initial_context = ["How", "are", "you?"]
+def preprocessing(freq_table):
+    # Filter comments of length < context_length
 
-# Filter comments of length < context_length
+    to_del = [] 
+    for k in freq_table.keys():    
+        if k[:4] == (27, 23893, 29, 198):
+            for i in range(4, len(k)):
+                if k[i:i+4] == (27, 23893, 29, 198):
+                    to_del.append(k)
 
-to_del = [] 
-for k in freq_table.keys():    
-    if k[:4] == (27, 23893, 29, 198):
-        for i in range(4, len(k)):
-            if k[i:i+4] == (27, 23893, 29, 198):
-                to_del.append(k)
-
-for k in to_del:
-    del freq_table[k]
-    print(f"Deleted {k}!")
+    for k in to_del:
+        del freq_table[k]
+        print(f"Deleted {k}!")
 
 
-if "Linux" in OS or "macOS" in OS:
-    os.system("clear")
-elif "Windows" in OS:
-   os.system("cls")
+    if "Linux" in OS or "macOS" in OS:
+        os.system("clear")
+    elif "Windows" in OS:
+        os.system("cls")
 
-# ------- FOR DEMO -------
-possible_starts = []
+def get_possible_starts(freq_table):
+    # ------- FOR DEMO -------
+    possible_starts = []
 
-for k,v in freq_table.items():
-    if k[:4] == (27, 23893, 29, 198):
-        possible_starts.append(k)
-    
-# -----------------------
+    for k,v in freq_table.items():
+        if k[:4] == (27, 23893, 29, 198):
+            possible_starts.append(k)
+        
+    # -----------------------
+    return possible_starts
 
-random.seed(1933)
-while(True):
-    initial_context = list(random.choice(possible_starts))
+def get_initial_context(possible_starts):
+    return list(random.choice(possible_starts))
+
+def generate_text(freq_table, initial_context):
+    #initial_context = ["How", "are", "you?"]
+
+
     generator = generate_tokens(freq_table, initial_context[-context_length:])
     tokens = list(generator)[4:-3]
     text = tokenizer.decode(tokens)
+    return text
+
+    """
     print(text)
-    a = input()
-    
-    if a == "q":
-        break
-    
+        
+        a = input()
+
+        if a == "q":
+            break
+        """
